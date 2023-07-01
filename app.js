bd5bc59bd741c16c08f621bfd2e935a6a31b56c9
Yashwanth Karuparthi <yashwanth782003@gmail.com>
2023-07-01 15:42:23 +0400

    4

--- app.js
+++ app.js
@@ -30,8 +30,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+// handlebars helpers
+const { formatDate } = require('./helpers/hbs')
+
 // handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs.engine({ helpers: {
+formatDate,
+},
+defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs')
 
 // sessions
